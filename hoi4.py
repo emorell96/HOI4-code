85b5f31154adc05c18a3886af33a58edc0f23371
emorell96 <emorell96@hotmail.com>
2020-06-26 11:53:46 -0700

    final commit of hoi4 python code

--- hoi4.py
+++ hoi4.py
@@ -27,10 +27,6 @@ for level in range(1, 2):
     Y = np.vectorize(lambda t: totalproduction(t, level, trade=trade_bonus))(T)/tank_production_cost
     plt.plot(T, Y, label=f"level: {level}, dispersed")
 
-total = 0
-for week in range(1, 52):
-    total += 50*(totalproduction(week*7, 3)-totalproduction((week-1)*7, 3))/13
-print(total)
 for level in range(1,2):
     Y = np.vectorize(lambda t: totalproduction(t, level, True, trade=trade_bonus))(T)
     plt.plot(T, Y/tank_production_cost, label=f"level: {level}, concentrated")
